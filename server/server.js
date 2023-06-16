6b6b1c463853b9be220018a7702b0d0dac6c754f
Teodor Simionescu <95678081+TeodorSim@users.noreply.github.com>
2023-06-17 01:30:21 +0300

    login to setting
    DEMO:
    cookies used to parse the username from login to the application

--- server/server.js
+++ server/server.js
@@ -7,6 +7,23 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
+  // Parse the "Cookie" header manually
+  const cookies = {};
+  if (req.headers.cookie) {
+    req.headers.cookie.split(';').forEach(cookie => {
+      const parts = cookie.split('=');
+      const name = parts[0].trim();
+      const value = parts[1].trim();
+      cookies[name] = value;
+    });
+  }
+
+  // Assign the parsed cookies to req.cookies
+  req.cookies = cookies;
+
+  // Access the parsed cookies
+  console.log('Cookies:', req.cookies);
+
   if (req.url === '/login' && req.method === 'GET') {
     // Serve the loginPage.html
     const filePath = path.join(__dirname, '../loginFile/loginPage.html');
@@ -59,7 +76,7 @@ const server = http.createServer((req, res) => {
         res.end(content);
       }
     });
-  }else if (req.url === '/favicon.svg' && req.method === 'GET') {
+  } else if (req.url === '/favicon.svg' && req.method === 'GET') {
     // Serve the cssTheme.css file
     const filePath = path.join(__dirname, 'img/icons/favicon.svg');
     fs.readFile(filePath, (err, content) => {
@@ -72,7 +89,7 @@ const server = http.createServer((req, res) => {
         res.end(content);
       }
     });
-  }else if (req.url === '/login' && req.method === 'POST') {
+  } else if (req.url === '/login' && req.method === 'POST') {
     // Handle the login form submission
     let body = '';
     req.on('data', (chunk) => {
@@ -89,12 +106,40 @@ const server = http.createServer((req, res) => {
           res.setHeader('Content-Type', 'application/json');
           res.end(JSON.stringify({ success: false }));
         } else {
-          res.statusCode = 200;
-          res.setHeader('Content-Type', 'application/json');
-          res.end(JSON.stringify({ success }));
+          if (success) {
+            // Save the username in a cookie
+            res.setHeader('Set-Cookie', `username=${username}`);
+            res.statusCode = 302;
+            res.setHeader('Location', '/settings');
+            res.end();
+          } else {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify({ success }));
+          }
         }
       });
     });
+  } else if (req.url === '/settings' && req.method === 'GET') {
+    const username = req.cookies.username;
+    if (username) {
+      // Serve the accountSettings.html
+      const filePath = path.join(__dirname, '../loginFile/accountSettings.html');
+      fs.readFile(filePath, (err, content) => {
+        if (err) {
+          res.statusCode = 500;
+          res.end('Internal Server Error');
+        } else {
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'text/html');
+          res.end(content);
+        }
+      });
+    } else {
+      res.statusCode = 302;
+      res.setHeader('Location', '/login');
+      res.end();
+    }
   } else if (req.url === '/homepage.html' && req.method === 'GET') {
     // Serve the homepage.html
     const filePath = path.join(__dirname, '../homepage.html');
@@ -108,7 +153,7 @@ const server = http.createServer((req, res) => {
         res.end(content);
       }
     });
-} else {
+  } else {
     // Handle other requests
     res.statusCode = 404;
     res.end('Not Found');
